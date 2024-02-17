35b3ff1bfad725902a10839c91ad18aa880b5415
qiusheng2011 <137812291@qq.com>
2024-02-17 18:10:33 +0800

    init project

---
+++ src/app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+
+
+app_description = """
+这一个独立的账户服务。\n
+提供登陆和注册的相关账户功能。\n
+其它服务不应当实现账户功能而是通过此服务获得 相应的账户id信息。
+
+"""
+
+
+app = FastAPI(
+    title="AccountAppServer",
+    description=app_description,
+    version='0.0.1',
+    openapi_url="/api/v1/openapi.json",
+    swagger_ui_parameters={"syntaxHighlight.theme":"monokai"},
+)
+
+
+
+@app.get("/")
+def root():
+    return {"message":"account appserver!"}
+
+
+@app.get("/health")
+def health():
+    return 
