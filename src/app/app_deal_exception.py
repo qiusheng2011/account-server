911e5f5e19b88ea5168eef3d2964661540062205
qiusheng2011 <137812291@qq.com>
2024-03-06 15:20:43 +0800

    update

--- src/app/app_deal_exception.py
+++ src/app/app_deal_exception.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 
 @appserver.exception_handler(DatabaseError)
 async def deal_db_database_error(request: Request, exc: Exception):
-    error_msg = f"数据库错误\t{request.url._url}\tsqlalchemy.errorcode={exc.code}\t{' '.join(exc.args)}"
+    error_msg = f"数据库错误\t{request.url._url}\tsqlalchemy.errorcode={
+        exc.code}\t{' '.join(exc.args)}"
     logger.error(error_msg)
     return PlainTextResponse("server error", status_code=500)
