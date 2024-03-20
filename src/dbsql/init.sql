89b27c8abaadd50fd342966b7531596ef41ec263
qiusheng2011 <137812291@qq.com>
2024-03-20 18:45:37 +0800

    fix: 文件重新命名未提交

--- src/dbsql/init.sql
+++ src/dbsql/init.sql
@@ -3,19 +3,20 @@ CREATE DATABASE IF NOT EXISTS tts CHARACTER
 SET = "utf8mb4" COLLATE = "utf8mb4_bin" COMMENT = "TTS";
 Use tts;
 -- 账户表
-CREATE TABLE IF NOT EXISTS accounts (
-    aid INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT "账户内在id",
-    email VARCHAR (50) NOT NULL UNIQUE KEY COMMENT "账户email",
-    account_name VARCHAR (20) NOT NULL UNIQUE KEY COMMENT "账户名称",
-    hash_password CHAR (64) NOT NULL COMMENT "账户密码hash值",
-    register_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT "注册时间"
+CREATE TABLE IF NOT EXISTS `accounts` (
+    `aid` INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT "账户内在id",
+    `email` VARCHAR (50) NOT NULL UNIQUE KEY COMMENT "账户email",
+    `account_name` VARCHAR (20) NOT NULL UNIQUE KEY COMMENT "账户名称",
+    `hash_password` CHAR (64) NOT NULL COMMENT "账户密码hash值",
+    `register_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT "注册时间"
 ) ENGINE = InnoDB COMMENT "账户表";
 -- 账户凭证表
-CREATE Table IF NOT EXISTS accounts_certificate_token (
-    aid INTEGER UNSIGNED PRIMARY KEY,
-    token char(64) NOT NULL,
-    refresh_token char(64) NOT NULL,
-    UNIQUE token (`token`),
-    UNIQUE refresh_token char(64) NOT NULL,
-    CONSTRAINT act_ibaid_1 FOREIGN KEY (`aid`) REFERENCES `accounts` (`aid`)
-) ENGINE = InnoDB COMMENT "账户凭证表";
+CREATE TABLE IF NOT EXISTS `accounts_certificate_token` (
+    `aid` int(10) unsigned NOT NULL,
+    `token` char(64) NOT NULL,
+    `refresh_token` char(64) NOT NULL,
+    PRIMARY KEY (`aid`),
+    UNIQUE KEY `token` (`token`),
+    UNIQUE KEY `refresh_token` (`refresh_token`),
+    CONSTRAINT `act_ibaid_1` FOREIGN KEY (`aid`) REFERENCES `accounts` (`aid`)
+) ENGINE = InnoDB COMMENT = '账户凭证表';
